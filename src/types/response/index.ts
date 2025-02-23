6770bcd6ecdc4be18cf9ffc99fa80310f289c9da
aon-sasawat <sasawat.supsinyothin@gmail.com>
2025-02-23 22:18:07 +0700

    feat: create type for get all response

--- src/types/response/index.ts
+++ src/types/response/index.ts
@@ -9,3 +9,29 @@ export interface SignInResponse extends BaseResponse {
     username: string;
   };
 }
+
+export interface GetAllTagsResponse extends BaseResponse {
+  data: {
+    id: string;
+    name: string;
+  }[];
+}
+
+export interface GetAllPostsResponse extends BaseResponse {
+  data: {
+    id: string;
+    title: string;
+    content: string;
+    userId: string;
+    tagId: string;
+    user: {
+      id: string;
+      username: string;
+    };
+    tag: {
+      id: string;
+      name: string;
+    };
+    comments: [];
+  }[];
+}
